d7c9c3b6c09eac12dd4a433de8eedec292e68bab
Daniel6702 <pedersendaniel3561@gmail.com>
2022-10-31 10:34:20 +0100

    Aflevering9

--- src/search.c
+++ src/search.c
@@ -3,5 +3,11 @@
 
 bool search(int a[], int n, int x)
 {
-    return 0;
+    if (n>0 && a[n-1] == x) { //if element is found return true
+        return true;
+    } else if (n == 0) { //if all element has been checked return false
+        return false;
+    } else {
+        search(a,n-1,x); //search again 
+    }
 }
--- src/sum.c
+++ src/sum.c
@@ -2,5 +2,7 @@
 
 int sum(int a[], int n)
 {
-    return 0;
+    int s = 0;
+    (n > 0) && (s = a[n-1] + sum(a,n-1));
+    return s;
 }
