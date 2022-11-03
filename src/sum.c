80f1be2ee4e2587c38b550f8265d1441b62c5933
Daniel670 <pedersendaniel3561@gmail.com>
2022-11-03 10:40:47 +0100

    sfghnsdfgh

--- src/sum.c
+++ src/sum.c
@@ -1,8 +1,24 @@
-#include "sum.h"
+//#include "sum.h"
+#include <stdio.h>
 
 int sum(int a[], int n)
 {
     int s = 0;
     (n > 0) && (s = a[n-1] + sum(a,n-1));
     return s;
+}
+
+int gcd(int a, int b) {
+    if (a == b) {
+        return a;
+    } else if (a>b) {
+        gcd(a-b,b);
+    } else if (a<b) {
+        gcd(a,b-a);
+    }
+}
+
+int main(void) {
+    printf("%d",gcd(16,8));
+    return 0;
 }
